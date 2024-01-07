94ee753708014b16eafd1238d997ec0d9672fbfa
jumli_d <148221429+jumli404@users.noreply.github.com>
2024-01-07 20:57:55 +0530

    Update script.js // create a object to create circle

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-  var canvas = document.getElementsByTagName('canvas')[0]
+
+
+
+
+        var canvas = document.getElementsByTagName('canvas')[0]
         var c = canvas.getContext('2d')
-        var v = 4
-        var G = 5
-        var x = 200
-        var y = 200
+        /*  var v = 4
+          var G = 5
+          var x = 200
+          var y = 200*/
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
+        function Opac(x, y) {
+            this.x = x
+            this.y = y
+            this.g = 4
+            this.v = 4
+            this.r = 50
+            this.draw = () => {
 
+                //  console.log(this.y)
+                c.beginPath()
+                c.clearRect(0, 0, innerWidth, innerHeight)
+                c.arc(this.x, this.y, this.r, Math.PI * 2, false)
+                c.strokeStyle = 'red'
+                c.fillStyle = 'red'
+                c.fill()
+                c.stroke()
+                this.move()
+                //console.log('a')
+            }
+            this.move = () => {
+                this.y += this.g
+                if (this.y + this.r > innerHeight || this.y - 50 < 0) {
+                    this.g = -this.g
+                    console.log(this.y)
+
+                    //this.x -= this.v
+                }
+                if (this.x + this.r > innerWidth || this.x - 50 < 0) {
+                    //  console.log("h")
+                    this.v = -this.v
+                    //alert("t")
+                }
+                this.x += this.v
+
+            }
+        }
+        var o = new Opac(200, 200)
         function ani() {
 
-            y += G
-            c.beginPath()
-            c.clearRect(0, 0, innerWidth, innerHeight)
-            c.arc(x, y, 50, Math.PI * 2, false)
-            c.strokeStyle = 'red'
-            c.fillStyle = 'red'
-            c.fill()
-            c.stroke()
-            if (y + 50 > innerHeight || y - 50 < 0) {
-                G = -G
-                // x -= v
-            }
-            if (x + 50 > innerWidth || x - 50 < 0) {
-                //  console.log("h")
-                v = -v
-            }
-            x += v
-            // console.log(y)
+
+            o.draw()
 
             requestAnimationFrame(ani)
         } ani()
+
